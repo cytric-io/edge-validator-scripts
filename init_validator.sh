23fbb24a57e3b4b91c771f09b47d2d1a2104daa2
kash <kash@cytric.io>
2023-12-17 19:43:28 +0100

    modified init_validator.sh

--- init_validator.sh
+++ init_validator.sh
@@ -19,20 +19,7 @@ source ~/.profile
 # Clone and build polygons-sdk
 git clone https://github.com/0xPolygon/polygon-edge && cd polygon-edge
 echo "Building Go executable, please wait..."
-go build main.go
+go build -o polygon-edge main.go
 
-# Initialize validator dir
-echo "Initializing validator directory.."
-cp ../genesis.json . && chmod +x genesis.json
-echo
-
-## Show private key
-pk=$(cat polygon-edge/consensus/validator.key)
-echo "Private Key = $pk"
-echo
-
-## Prompt user
-echo
-echo
-read -n 1 -s -r -p "Additionally, please make a copy of your \`Private Key\` and store this somewhere safe - DO NOT share it with us! Once this is done, press any key to continue.."
-echo
+# Move the binary to a directory in your PATH
+sudo mv polygon-edge /usr/local/bin/
