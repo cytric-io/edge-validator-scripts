772970533ac5c84ce32f4100afc95a17139488ed
kash <kash@cytric.io>
2023-12-22 01:53:11 +0100

    Added installation of nvim

--- init_validator.sh
+++ init_validator.sh
@@ -11,6 +11,16 @@ echo
 
 # Install dependencies
 sudo apt install wget
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+# Install Go
 wget https://golang.org/dl/go1.19.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
